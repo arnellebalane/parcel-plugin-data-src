2164b0f716c7bb0e46ce3572884b4c016c77ba05
Arnelle Balane <arnellebalane@gmail.com>
2018-12-02 13:03:36 +0800

    Fix invalid arg to addAssetType calls

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const HTMLAsset = require('./html-asset');
-
 module.exports = bundler => {
-    bundler.addAssetType('html', HTMLAsset);
-    bundler.addAssetType('htm', HTMLAsset);
+    bundler.addAssetType('html', require.resolve('./html-asset'));
+    bundler.addAssetType('htm', require.resolve('./html-asset'));
 };
